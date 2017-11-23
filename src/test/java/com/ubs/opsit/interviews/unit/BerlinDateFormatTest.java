1ea20961a55bbbac85641fae1d0321c2fe9208b6
alexisgayte <alexis.gayte@gmail.com>
2017-11-23 19:55:28 +0000

    formation

--- src/test/java/com/ubs/opsit/interviews/unit/BerlinDateFormatTest.java
+++ src/test/java/com/ubs/opsit/interviews/unit/BerlinDateFormatTest.java
@@ -27,25 +27,23 @@ public class BerlinDateFormatTest {
 	@Test
 	public void zeroHourTest() throws ParseException {
 
-		String format = berlinClockTime.format(new Date(0,0,0,0,0,0));
+		String format = berlinClockTime.format(new Date(0, 0, 0, 0, 0, 0));
 
 		assertEquals("Y\nOOOO\nOOOO\nOOOOOOOOOOO\nOOOO", format);
 	}
 
-
 	@Test
 	public void tenHourTest() throws ParseException {
 
-		String format = berlinClockTime.format(new Date(0,0,0,10,0,0));
+		String format = berlinClockTime.format(new Date(0, 0, 0, 10, 0, 0));
 
 		assertEquals("Y\nRROO\nOOOO\nOOOOOOOOOOO\nOOOO", format);
 	}
 
-
 	@Test
 	public void normalTwentyFourHourTest() throws ParseException {
 
-		String format = berlinClockTime.format(new Date(0,0,0,24,0,0));
+		String format = berlinClockTime.format(new Date(0, 0, 0, 24, 0, 0));
 
 		assertEquals("Y\nOOOO\nOOOO\nOOOOOOOOOOO\nOOOO", format);
 	}
@@ -53,9 +51,9 @@ public class BerlinDateFormatTest {
 	@Test
 	public void twentyFourHourSpecialLogicTest() throws ParseException {
 
-		berlinClockTime =  new BerlinDateFormat(true);
+		berlinClockTime = new BerlinDateFormat(true);
 
-		String format = berlinClockTime.format(new Date(0,0,2,0,0,0));
+		String format = berlinClockTime.format(new Date(0, 0, 2, 0, 0, 0));
 
 		assertEquals("Y\nRRRR\nRRRR\nOOOOOOOOOOO\nOOOO", format);
 	}
@@ -71,7 +69,7 @@ public class BerlinDateFormatTest {
 	public void cloneTest() {
 
 		berlinClockTime = (BerlinDateFormat) berlinClockTime.clone();
-		String format = berlinClockTime.format(new Date(0,0,0,0,0,0));
+		String format = berlinClockTime.format(new Date(0, 0, 0, 0, 0, 0));
 
 		assertEquals("Y\nOOOO\nOOOO\nOOOOOOOOOOO\nOOOO", format);
 
